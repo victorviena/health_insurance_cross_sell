24bada6596b09a0339a41241139dd7183ba11b5a
Victor Viena <130564248+victorviena@users.noreply.github.com>
2023-07-12 12:24:12 -0300

    Update HealthInsurance.py

--- app/models/HealthInsurance.py
+++ app/models/HealthInsurance.py
@@ -6,8 +6,8 @@ import pandas as pd
 class HealthInsurance:
     
     def __init__( self ):
-        self.home_path =''
-        self.gender_encoding = pickle.load( open( '/features/gender_encoding.pkl', 'rb' ) )
+        self.home_path = ''
+        self.gender_encoding = pickle.load( open( self.home_path + '\features\gender_encoding.pkl', 'rb' ) )
         self.vehicle_damage_encoding = pickle.load( open( self.home_path + '\features\vehicle_damage_encoding.pkl', 'rb' ) ) 
         self.vehicle_age_encoding = pickle.load( open( self.home_path + '\features\vehicle_age_encoding.pkl', 'rb' ) )
         self.region_code_encoding = pickle.load( open( self.home_path + '\features\region_code_encoding.pkl', 'rb' ) )
